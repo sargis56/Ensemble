aa76de87c86764f36d48e2803c6dc75605ab2d0e
Daniel Stokes <danielphilipstokes@gmail.com>
2018-11-12 17:18:14 -0500

    heroku live websocket fix 13

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ var generateRandomString = function (length) {
     return text;
 };
 
-const wss = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ app });
 
 
 wss.on('connection', function connection(ws) {
